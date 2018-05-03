510968c790dd6594325f4b2dc121f8b8afd31bda
dodostefani <domenico.stefani@studenti.unitn.it>
2018-05-03 22:12:01 +0200

    not entirely working

--- cinema/src/main/java/it/unitn/disi/cinema/servlets/MainServlet.java
+++ cinema/src/main/java/it/unitn/disi/cinema/servlets/MainServlet.java
@@ -13,6 +13,7 @@ import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -87,7 +88,8 @@ public class MainServlet extends HttpServlet {
 				request.setAttribute("film", film);
 				request.setAttribute("genere", genere);
 				request.setAttribute("spettacoli", spettacoliDisponibili);
-                   
+				request.setAttribute("calltime", now);/**/
+                
 
 				request.setAttribute("message","La richiesta arriva dalla servlet");
                 
